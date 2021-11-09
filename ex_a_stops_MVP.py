f7ed3665121bad74f37356b58865edcc4edc3bd1
Derek Reid <itsjustdel@gmail.com>
2021-11-09 17:37:04 +0000

    Formatting

--- ex_a_stops_MVP.py
+++ ex_a_stops_MVP.py
@@ -2,22 +2,31 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #1. Add "Edinburgh Waverley" to the end of the list
 stops.append("Edinburgh Waverly")
+
 #2. Add "Glasgow Queen St" to the start of the list
 stops.insert(0, "Glasgow Queen St")
+
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert( stops.index("Falkirk High") + 1, "Polmont")
+
 #4. Print out the index position of "Linlithgow"
 print(stops.index("Linlithgow"))
+
 #5. Remove "Livingston" from the list using its name
 stops.remove("Linlithgow")
+
 #6. Delete "Cumbernauld" from the list by index
 stops.pop(stops.index("Cumbernauld"))
+
 #7. Print the number of stops there are in the list
 print(len(stops))
+
 #8. Sort the list alphabetically
 stops.sort()
+
 #9. Reverse the positions of the stops in the list
 stops.reverse()
+s
 #10 Print out all the stops using a for loop
 for stop in stops:
     print(stop)
